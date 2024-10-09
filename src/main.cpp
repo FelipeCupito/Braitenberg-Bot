4af0e85ea7dd9200d402136dba1eefb296d36871
Felipe Cupitó <65612488+FelipeCupito@users.noreply.github.com>
2024-10-09 15:26:44 -0300

    Implement changes in main

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "Motor.h"
 #include "MotorController.h"
 #include "Sonar.h"
+#include "LightSensor.h"
 
 // Se perdio PWM de los pines 3 y 11 por usar NewPing non-blocking
 //  Motores
@@ -19,8 +20,18 @@
 // Ultrasonico
 #define ULTRA_SENSOR_RIGHT A0  // Amarillo
 #define ULTRA_SENSOR_LEFT A1   // Marrón 
-#define MAX_DISTANCE 100
-#define MIN_DISTANCE 0 
+#define MAX_DISTANCE 50
+
+// Light Sensors (Analog Pins)
+const int LIGHT_SENSOR_PINS[LightSensor::NUM_SENSORS] = {
+    A2, // FRONT_LEFT
+    A3, // FRONT_RIGHT
+    A4, // LEFT
+    A5, // RIGHT
+    A6  // REAR
+};
+#define MAX_LIGHT_INTENSITY 1023
+#define MIN_LIGHT_INTENSITY 0
 
 // Distancia
 #define CRITICAL_DISTANCE 20
@@ -32,30 +43,37 @@
 #define MAX_VELOCITY 255   // Velocidad máxima
 #define MIN_VELOCITY 100     // Velocidad mínima para giros
 
-
+// Motors Instances
 Motor motorLeft(MOTOR_PIN_ENA, MOTOR_PIN_IN1, MOTOR_PIN_IN2, Motor::INVERTED, MOTOR_CALIBRATION_LEFT);
 Motor motorRight(MOTOR_PIN_ENB, MOTOR_PIN_IN3, MOTOR_PIN_IN4, Motor::NORMAL, MOTOR_CALIBRATION_RIGHT);
 MotorController motorController(motorLeft, motorRight);
 
+// Sensors Instances
 Sonar sonar(ULTRA_SENSOR_RIGHT, ULTRA_SENSOR_LEFT, MAX_DISTANCE);
+LightSensor lightSensor(LIGHT_SENSOR_PINS);
 
 void setup() {
   Serial.begin(9600);
   
   motorController.setup();
-  motorController.setAdjustmentMethod(MotorController::SIGMOID_DIFFERENCE);
-
   sonar.setup();
+  lightSensor.setup();
 }
 
 void loop() {
   sonar.update();
+  lightSensor.update();
   
+  // Obtener distancia del sensor ultrasonico
   int distance = sonar.getDistance();
-  // int newMaxVelocity = constrain(map(distance, MIN_DISTANCE, MAX_DISTANCE, MIN_VELOCITY, MAX_VELOCITY), MIN_VELOCITY, MAX_VELOCITY);
-  // motorController.setMaxVelocity(newMaxVelocity);
 
+  // Obtener intensidades de los sensores frontales
+  int leftIntensity = lightSensor.getLightIntensity(LightSensor::FRONT_LEFT);
+  int rightIntensity = lightSensor.getLightIntensity(LightSensor::FRONT_RIGHT);
 
+  //TODO: aca se poria reducir la velocidad a medida que se acerca o se aleja de una medicion
+
+  // Comportamiento
   if (distance > 0 && distance <= CRITICAL_DISTANCE) {
     Serial.println("Obstáculo muy cerca, girando en el lugar");
     motorController.stop();
@@ -66,23 +84,26 @@ void loop() {
     }
     delay(1000);
   } 
-  else if (distance > CRITICAL_DISTANCE && distance <= AVOID_DISTANCE) {
-    Serial.println("Obstáculo detectado, evitando");
-    motorController.adjustSpeedsBasedOnObstacleDistances(sonar.getLeftDistance(), sonar.getRightDistance(), CRITICAL_DISTANCE, AVOID_DISTANCE);
+  // else if (distance > CRITICAL_DISTANCE && distance <= AVOID_DISTANCE) {
+  //   Serial.println("Obstáculo detectado, evitando");
+  //   motorController.adjustSpeedsBasedOnObstacleDistances(sonar.getLeftDistance(), sonar.getRightDistance(), CRITICAL_DISTANCE, AVOID_DISTANCE);
 
-    // if(Sonar::CloserSide::RIGHT == sonar.getCloserSide()){
+  //   // if(Sonar::CloserSide::RIGHT == sonar.getCloserSide()){
 
-    //   Serial.print("Girando a la izquierda, distancia: ");
-    //   Serial.println(distance);
-    //   motorController.turnLeftWithObstacleDistance(distance, CRITICAL_DISTANCE, AVOID_DISTANCE);
-    // } else {
-    //   Serial.print("Girando a la derecha, distancia: ");
-    //   Serial.println(distance);
-    //   motorController.turnRightWithObstacleDistance(distance, CRITICAL_DISTANCE, AVOID_DISTANCE);
-    // }
-  } 
+  //   //   Serial.print("Girando a la izquierda, distancia: ");
+  //   //   Serial.println(distance);
+  //   //   motorController.turnLeftWithObstacleDistance(distance, CRITICAL_DISTANCE, AVOID_DISTANCE);
+  //   // } else {
+  //   //   Serial.print("Girando a la derecha, distancia: ");
+  //   //   Serial.println(distance);
+  //   //   motorController.turnRightWithObstacleDistance(distance, CRITICAL_DISTANCE, AVOID_DISTANCE);
+  //   // }
+  // } 
   else {
     Serial.println("No hay obstáculo, avanzando");
-    motorController.moveForward(MAX_VELOCITY); 
-  }   
+    motorController.adjustSpeedsToApproach(
+      leftIntensity, rightIntensity, 
+      MIN_LIGHT_INTENSITY, MAX_LIGHT_INTENSITY,
+      MotorController::SIGMOID_DIFFERENCE);
+    }   
 }
